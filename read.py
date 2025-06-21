22fb9f3f1ec96cbbd76bcdfdb400161cf474ad73
saito40 <80165008+Saito40@users.noreply.github.com>
2025-06-22 02:48:48 +0900

    pahoを2に移行テスト

--- read.py
+++ read.py
@@ -20,13 +20,13 @@ font = pygame.font.Font(None, 36)
 
 
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(client, userdata, flags, rc, properties):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
+    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
     # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
